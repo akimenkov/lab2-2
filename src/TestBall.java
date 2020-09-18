17ac62553776ddf1dcf6e58778f31e16461c47e4
j1ze1973 <thesevchiktel@gmail.com>
2020-09-18 10:56:55 +0300

    Initial commit

---
+++ src/TestBall.java
@@ -0,0 +1,8 @@
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(250, 250);
+        System.out.println(b1);
+        b1.move(45, 30);
+        System.out.println(b1);
+    }
+}
